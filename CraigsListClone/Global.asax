c2c8d1fc8ce8c5c83357d50096a75c323fabcc5f
ksindorf000 <ksindorf000@gmail.com>
2017-03-06 13:49:00 -0500

    Add project files.

---
+++ CraigsListClone/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CraigsListClone.MvcApplication" Language="C#" %>
---
+++ CraigsListClone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CraigsListClone.Startup))]
+namespace CraigsListClone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
